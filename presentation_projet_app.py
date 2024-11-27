b23b466f0e739e9efdc6aa5272e6836489d25f6f
mathildepmr <mathilde.paumier@etu.unilasalle.fr>
2024-11-27 21:51:29 +0100

    Update presentation_projet_app.py

--- presentation_projet_app.py
+++ presentation_projet_app.py
@@ -15,7 +15,7 @@ elif page == "Problématique et data set":
    st.subheader(":green[Étude de l'impact de La guerre en Ukraine sur la production d'énergie renouvelable en France en fonction des années ]")
    st.subheader(" ", divider="gray")
    st.subheader("Présentation du dataset")
-   data = pd.read_csv(fichierdata)
+   st.dataframe(fichierdata)
   
  
 elif page == "Code python":
