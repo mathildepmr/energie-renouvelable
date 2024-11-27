6459357025e2b44b7fba0511497f3d797ab5c932
mathildepmr <mathilde.paumier@etu.unilasalle.fr>
2024-11-27 21:46:05 +0100

    Update presentation_projet_app.py

--- presentation_projet_app.py
+++ presentation_projet_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.datasets import 
+from sklearn.datasets import prod-region-annuelle-encr.cv
 
 page = st.sidebar.radio("Naviguer entre les pages :", ["Introduction", "Problématique et data set", "Code python","Graphique", "Conclusion"])
 
@@ -14,8 +14,8 @@ elif page == "Problématique et data set":
    st.subheader(":green[Étude de l'impact de La guerre en Ukraine sur la production d'énergie renouvelable en France en fonction des années ]")
    st.subheader(" ", divider="gray")
    st.subheader("Présentation du dataset")
-
-   
+  
+ 
 elif page == "Code python":
     st.header("Code Python")
     st.write("Ajoutez ici votre contenu pour la première page.")
