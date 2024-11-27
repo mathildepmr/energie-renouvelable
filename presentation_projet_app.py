226cee866299ddd4cb399b8321a4a3be58d526ca
mathildepmr <mathilde.paumier@etu.unilasalle.fr>
2024-11-27 21:16:47 +0100

    Update presentation_projet_app.py

--- presentation_projet_app.py
+++ presentation_projet_app.py
@@ -2,22 +2,17 @@ import streamlit as st
 import pandas as pd
 
 
-st.title('Projet Python') #page 1
-st.markdown('''Mathilde Paumier, Antony Riedberger, Nour Karam''')
-st.header("Problématique:") 
-st.subheader(":green[Étude de l'impact de La guerre en Ukraine sur la production d'énergie renouvelable en France en fonction des années ]")
+def page_1():
+    st.title('Projet Python') #page 1
+    st.markdown('''Mathilde Paumier, Antony Riedberger, Nour Karam''')
+    st.header("Problématique:") 
+    st.subheader(":green[Étude de l'impact de La guerre en Ukraine sur la production d'énergie renouvelable en France en fonction des années ]")
+    st.subheader(" ", divider="gray")
+    st.subheader("Présentation du dataset")
 
+def page_2():
+    st.title("Code Python ")
 
-st.subheader(" ", divider="gray")
-st.subheader("Présentation du dataset")
-
-def page2():
-    st.title("Code python")
-
-pg = st.navigation([
-    st.Page("page1.py", title="Introduction", icon="📝"),
-    st.Page(page2, title="Code python", icon=":material/favorite:"),
-])
+pg = st.navigation([st.Page("page_1.py"), st.Page(page_2)])
 pg.run()
-
 
