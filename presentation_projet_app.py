5df02b1cb8aa99a224595288ad53b31c0841722b
mathildepmr <mathilde.paumier@etu.unilasalle.fr>
2024-11-27 21:00:05 +0100

    Update presentation_projet_app.py

--- presentation_projet_app.py
+++ presentation_projet_app.py
@@ -1,15 +1,10 @@
 import streamlit as st
 import pandas as pd
-from sklearn.datasets import prod-region-annuelle-enr.csv
+
 
 st.title('Projet Python')
 st.header("Problématique:") 
 st.subheader(":green[Étude de l'impact de La guerre en Ukraine sur la production d'énergie renouvelable en France en fonction des années ]")
 
-df = pd.DataFrame(values, columns=feature_names)
-df
-plot = df['energie renouvelable']
-
-if bot:
-    st.line_chart(plot)
-boutondata = st.checkbox("afficher le data set")
+st.subheader(" ", divider="gray")
+st.subheader("Présentation du dataset")
