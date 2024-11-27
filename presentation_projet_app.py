98ada74bdf055ae29837b60a5c4ad03beb6162cc
mathildepmr <mathilde.paumier@etu.unilasalle.fr>
2024-11-27 23:03:48 +0100

    Update presentation_projet_app.py

--- presentation_projet_app.py
+++ presentation_projet_app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 
 data = "https://github.com/mathildepmr/energie-renouvelable/blob/main/prod-region-annuelle-enr.csv"
-  
-  ^
-IndentationError: unexpected unindent
+
 page = st.sidebar.radio("Naviguer entre les pages :", ["Introduction", "Problématique et data set", "Code python","Graphique", "Conclusion"])
 
 if page == "Introduction":
