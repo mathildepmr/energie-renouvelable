38874804842c8f9790a935adb455d21a53062cdd
mathildepmr <mathilde.paumier@etu.unilasalle.fr>
2024-11-27 21:09:17 +0100

    Update presentation_projet_app.py

--- presentation_projet_app.py
+++ presentation_projet_app.py
@@ -10,3 +10,14 @@ st.subheader(":green[Étude de l'impact de La guerre en Ukraine sur la productio
 
 st.subheader(" ", divider="gray")
 st.subheader("Présentation du dataset")
+
+def page2():
+    st.title("Code python")
+
+pg = st.navigation([
+    st.Page("page1.py", title="Introduction", icon="📝"),
+    st.Page(page2, title="Code python", icon=":material/favorite:"),
+])
+pg.run()
+
+
