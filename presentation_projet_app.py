3c7db1dec518a84f87bd4b39d211f75f67efdbc3
mathildepmr <mathilde.paumier@etu.unilasalle.fr>
2024-11-27 21:25:52 +0100

    Update presentation_projet_app.py

--- presentation_projet_app.py
+++ presentation_projet_app.py
@@ -2,29 +2,31 @@ import streamlit as st
 import pandas as pd
 
 
-
-st.title('Projet Python') 
-st.markdown('''Mathilde Paumier, Antony Riedberger, Nour Karam''')
-st.header("Problématique:") 
-st.subheader(":green[Étude de l'impact de La guerre en Ukraine sur la production d'énergie renouvelable en France en fonction des années ]")
-st.subheader(" ", divider="gray")
-st.subheader("Présentation du dataset")
-
 page = st.sidebar.radio("Naviguer entre les pages :", ["Accueil", "Page 1", "Page 2"])
 
 if page == "Accueil":
-    st.header("Bienvenue sur la page d'accueil !")
-    st.write("Cette application contient plusieurs pages. Utilisez le menu pour naviguer.")
+   st.title('Projet Python') 
+    st.markdown('''Mathilde Paumier, Antony Riedberger, Nour Karam''')
+    st.header("Problématique:") 
+    st.subheader(":green[Étude de l'impact de La guerre en Ukraine sur la production d'énergie renouvelable en France en fonction des années ]")
+    st.subheader(" ", divider="gray")
+    st.subheader("Présentation du dataset")
 
 
-elif page == "Page 1":
+elif page == "Code python":
     st.header("Page 1 : Visualisation")
     st.write("Ajoutez ici votre contenu pour la première page.")
     # Exemple de graphique
     st.line_chart([1, 2, 3, 4, 5])
 
 
-elif page == "Page 2":
+elif page == "Graphique":
+    st.header("Page 2 : Analyse")
+    st.write("Ajoutez ici votre contenu pour la deuxième page.")
+    # Exemple de formulaire
+    user_input = st.text_input("Entrez quelque chose :")
+
+elif page == "Conclusion":
     st.header("Page 2 : Analyse")
     st.write("Ajoutez ici votre contenu pour la deuxième page.")
     # Exemple de formulaire
