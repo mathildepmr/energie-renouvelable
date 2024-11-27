86fe1e2a145487dc30df002639f670d83903e481
mathildepmr <mathilde.paumier@etu.unilasalle.fr>
2024-11-27 21:17:27 +0100

    Update presentation_projet_app.py

--- presentation_projet_app.py
+++ presentation_projet_app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 
 
-def page_1():
-    st.title('Projet Python') #page 1
-    st.markdown('''Mathilde Paumier, Antony Riedberger, Nour Karam''')
-    st.header("Problématique:") 
-    st.subheader(":green[Étude de l'impact de La guerre en Ukraine sur la production d'énergie renouvelable en France en fonction des années ]")
-    st.subheader(" ", divider="gray")
-    st.subheader("Présentation du dataset")
+
+st.title('Projet Python') #page 1
+st.markdown('''Mathilde Paumier, Antony Riedberger, Nour Karam''')
+st.header("Problématique:") 
+st.subheader(":green[Étude de l'impact de La guerre en Ukraine sur la production d'énergie renouvelable en France en fonction des années ]")
+st.subheader(" ", divider="gray")
+st.subheader("Présentation du dataset")
 
 def page_2():
     st.title("Code Python ")
