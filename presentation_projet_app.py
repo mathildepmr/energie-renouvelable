e64b35adc1c031b787fa53be1f4a32f18862dbf6
mathildepmr <mathilde.paumier@etu.unilasalle.fr>
2024-11-27 20:11:23 +0100

    Update presentation_projet_app.py

--- presentation_projet_app.py
+++ presentation_projet_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Energie renouvelable')
+st.title('Projet Python')
+st.header("Problématique :green[Étude de l'impact de La guerre en Ukraine sur la production d'énergie renouvelable en France en fonction des années ]",divider="gray)
+
+st.header("data set ", divider=True)
